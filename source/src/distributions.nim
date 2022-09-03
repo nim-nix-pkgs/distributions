65add93ae6a6b50c30755368a448dfee374b72fe
hugosenari <hugosenari@gmail.com>
2022-09-03 19:47:37 -0300

    feat(nimpkgs): update lock

--- source/src/distributions.nim
+++
@@ -1,2 +0,0 @@
-import distributions/[bernoulli, beta, binomial, chi2, f, gamma, negativebinomial, normal, poisson, t, uniformcontinuous, uniformdiscrete, mathutils]
-export bernoulli, beta, binomial, chi2, f, gamma, negativebinomial, normal, poisson, t, uniformcontinuous, uniformdiscrete, mathutils
--- source/src/distributions/base.nim
+++
@@ -1,5 +0,0 @@
-{.nanChecks: on, infChecks: on.}
-type
-  Distribution* = object of RootObj
-  DistributionContinuous* = object of Distribution
-  DistributionDiscrete* = object of Distribution
